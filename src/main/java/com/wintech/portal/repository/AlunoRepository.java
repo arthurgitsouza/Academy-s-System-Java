57bf9d56755136905a00b16497d7e9fe2d10bee1
arthurgitsouza <arthurgitsouza@gmail.com>
2025-11-23 00:34:58 -0300

    eu(arthur) terminando 00:34 com thai(diva) o back-end

--- src/main/java/com/wintech/portal/repository/AlunoRepository.java
+++ src/main/java/com/wintech/portal/repository/AlunoRepository.java
@@ -3,12 +3,12 @@ package com.wintech.portal.repository;
 import com.wintech.portal.domain.Aluno;
 import com.wintech.portal.domain.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-
+public interface AlunoRepository extends JpaRepository<Aluno, Long>, JpaSpecificationExecutor<Aluno> {
     // ADICIONE ESTE MÉTODO
     List<Aluno> findByTurma(Turma turma);
 }
